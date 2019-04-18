b1245e5f62b2fdb4e0d04f23c2762622e50efe20
Max Redmond <athulus1991@gmail.com>
2019-04-18 19:33:16 -0400

    splitting out the individual and horde functions from the cli

--- treasure.py
+++ treasure.py
@@ -3,6 +3,9 @@ import random
 import re
 import operator
 
+def formatCoins(coins):
+    return f"{coins[0]} copper pieces, {coins[1]} silver pieces, {coins[2]} 'electrum' pieces, {coins[3]} gold pieces, {coins[4]} platinum pieces"
+
 def parseCoin(expression: str):
     quantity, *e = re.split("[dx]",expression)
     quantity = int(quantity)
@@ -55,49 +58,55 @@ def parseArt(expression:str):
         sum += random.randrange(1,diesize+1)
     return f"A {description} worth {sum * multiplier} pieces of gold"
 
-def parseMagicItems(expression:str):
-    quantity, diesize, magicTable = re.split("[dx]",expression)
-    quantity = int(quantity)
-    diesize = int(diesize)
-    sum = 0
-    for i in range(quantity):
-        sum += random.randrange(1,diesize+1)
+def parseMagicItems(expressions:list):
     items = ""
-    for i in range(sum):
-        d100 = random.randrange(100)
-        items += tables.magicItems[magicTable][d100] + "\n"
+    for expression in expressions:
+        quantity, diesize, magicTable = re.split("[dx]",expression)
+        quantity = int(quantity)
+        diesize = int(diesize)
+        sum = 0
+        for i in range(quantity):
+            sum += random.randrange(1,diesize+1)
+        for i in range(sum):
+            d100 = random.randrange(100)
+            items += tables.magicItems[magicTable][d100] + "\n"
     return items
 
-def horde():
-    cr = int(input("what is the CR you are using to determine this hoard? integers between 0 and 25 only please "))
+def horde(cr):
     coins = parseCoins(tables.hoard[cr]["coins"])
     d100 = random.randrange(100)
     art = parseArt(tables.hoard[cr]['valuables'][d100]['art'])
     gems = parseGems(tables.hoard[cr]['valuables'][d100]['gems'])
     items = parseMagicItems(tables.hoard[cr]['valuables'][d100]['magic'])
-    #TODO form the output into a pretty string
-    print(coins)
-    print(art)
-    print(gems)
-    print(items)
+    return f"coinage: {formatCoins(coins)}\n{art}\n{gems}\nMagic stuff:\n{items}"
 
 
 
-def individual(coins = [0,0,0,0,0]):
-    cr = int(input("what is the CR of your creature? integers between 0 and 25 only please "))
-    quantity = int(input("how may creatures are CR " + str(cr)))
-    for i in range(quantity):
-        d100 = random.randrange(100)
-        coinExpressions = tables.individual[cr][d100]
-        coins = list(map(operator.add, coins, parseCoins(coinExpressions)))
-    end = input("More creatures? ")
-    if end == "yes":
-        individual(coins)
-        return
-    print(f"{coins[0]} copper pieces, {coins[1]} silver pieces, {coins[2]} 'electrum' pieces, {coins[3]} gold pieces, {coins[4]} platinum pieces")
+def individual(creatureSets):
+    coins = [0,0,0,0,0]
+    for creature in creatureSets:
+        cr, quantity = creature
+        for i in range(quantity):
+            d100 = random.randrange(100)
+            coinExpressions = tables.individual[cr][d100]
+            coins = list(map(operator.add, coins, parseCoins(coinExpressions)))
+    return formatCoins(coins)
 
-treasure_type = input("individual or horde?(h/i)")
-if treasure_type == "h":
-    horde()
-else:
-    individual()
+if __name__ == "__main__":
+    treasure_type = input("individual or horde?(h/i)")
+    if treasure_type == "h":
+        cr = int(input("what is the CR you are using to determine this hoard? integers between 0 and 25 only please "))
+        print(horde(cr))
+    else:
+        creatureSets = []
+        more = True
+        while(more):
+            cr = int(input("what is the CR of your creature? integers between 0 and 25 only please "))
+            quantity = int(input("how may creatures are CR " + str(cr)))
+            creatureSets.append((cr,quantity))
+            answer = input("More creatures? (y/n)")
+            if answer == "y":
+                more = True
+            else:
+                more = False
+        print(individual(creatureSets))
